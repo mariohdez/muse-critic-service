3c20f5739ee310a29502024d18fcc50c803350fe
Mario Hernandez <mhmario963@gmail.com>
2022-11-21 13:00:05 -0700

    Add home feed controller stub.

---
+++ MuseCritic/MuseCritic/Controllers/HomeFeedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MuseCritic.Models;
+using MuseCritic.Repository;
+
+namespace MuseCritic.Controllers
+{
+    [Route("api/homefeed")]
+    public class HomeFeedController : ControllerBase
+    {
+    }
+}
+
